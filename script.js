d589354bc28c6b8f2c1b5340d6cf093327b3ed0e
atiks88 <141978852+atiks88@users.noreply.github.com>
2024-01-06 23:29:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let student = {
+	name:atik;
+}
+
+
+
+object.prototype.getKeys=function(){
+	return Object.keys(this);
+};
+
+let key = student.getKeys();
+
+console.log(key);
